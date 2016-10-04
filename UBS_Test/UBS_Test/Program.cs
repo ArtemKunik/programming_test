e5002eae2e2e8f5c2f57eb809182297e0538865e
Artem Kunyk <artemkunik@gmail.com>
2016-10-04 10:24:46 +0000

    done

---
+++ UBS_Test/UBS_Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Programming_Test
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            string sentence = "This is a statement, and so is this";
+
+            foreach (var word in sentence.Split(new[] {'.', '?', '!', ' ', ';', ':', ','},
+                    StringSplitOptions.RemoveEmptyEntries)
+                .GroupBy(g => g.ToLowerInvariant())
+                .Select(g => new {Word = g.Key, count = g.Count()}))
+                Console.WriteLine($"{word.Word} - {word.count}");
+            Console.ReadKey();
+        }
+    }
+}
